000ae59e0a7bddccb46f6ba9aa7bcdd274e6a6bf
Waznygx <135837095+Waznygx@users.noreply.github.com>
2023-09-22 21:33:55 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+
+//#define M 100
+//int main()
+//{
+//	int a = M;
+////#undef M
+//	printf("%d", M);
+//	return 0;
+//}
+//#define MAX 10
+//int main()
+//{
+//	int arr[MAX] = { 0 };
+//	int i = 0;
+//	for ( i = 0; i < MAX; i++)
+//	{
+//		arr[i] = i;\
+//		printf("%d ", arr[i]);
+//
+//	}
+//	return 0;
+//}
+
+//#define DEBUG
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 0 };
+//	for ( i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//#ifdef DEBUG
+//		printf("%d ", arr[i]);
+//#endif // DEBUG
+//	}
+//	return 0;
+//}
+
+//#define haha
+//#define heihei
+//int main()
+//{
+//#ifdef haha
+//	printf("haha1\n");
+//#endif // haha
+//#if defined haha
+//	printf("haha2\n");
+//#endif
+//
+//#ifndef heihei
+//	printf("heihei1\n");
+//#endif // !heihei
+//#if !defined heihei
+//	printf("heihei2\n");
+//#endif // !defined heihei
+////#if 1==0
+////	printf("true\n");
+////#elif 1==1
+////	printf("2\n");
+////#else
+////	printf("false\n");
+////#endif // 1==1
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 0 };
+//	for ( i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+int main() 
+{
+    printf("-- %s: %d -- \\n", __FILE__, __LINE__);
+#line 81 "foo.c"
+    printf("-- %s: %d -- \\n", __FILE__, __LINE__);
+#line 101 "foo.c"
+    printf("-- %s: %d -- \\n", __FILE__, __LINE__);
+    return 0;
+}
